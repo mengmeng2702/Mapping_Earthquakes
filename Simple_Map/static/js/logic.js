98f91d4f8997e2128ced3246e8dee001d23a361c
Mengdi Liu <liumengdi923@gmail.com>
2021-03-21 21:07:37 -0400

    changed map street style to dark and added a circle marker

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,10 +2,19 @@
 console.log("working");
 
 //create the map object with a  center and zoom level.
-let map = L.map("mapid").setView([40.7, -94.5],4);
+let map = L.map("mapid").setView([34.0522, -118.2437],14);
+
+//add a marker to the map for los angeles, california
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+L.circleMarker([34.0522, -118.2437],{
+    radius: 300,
+    color: "black",
+    fillColor: "#ffffa1"
+}).addTo(map);
 
 //create the tile layer that will be the bakcground of our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
