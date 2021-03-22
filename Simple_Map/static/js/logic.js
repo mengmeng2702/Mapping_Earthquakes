6c3a34d14578ee5d3c4cb74e6ca06cefcb69dd3a
Mengdi Liu <liumengdi923@gmail.com>
2021-03-22 00:13:09 -0400

    changed the logic.js to add polyline to the map

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -2,35 +2,32 @@
 console.log("working");
 
 //create the map object with a  center and zoom level.
-let map = L.map("mapid").setView([29.7604, -95.3698],4);
+let map = L.map("mapid").setView([36.1733, -96.5400],5);
 
-//add a marker to the map for los angeles, california
-//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//coordinates for each point to be used in the line.
+let line =[
+    [33.9416, -118.4085],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
+];
 
-//get data from cities.js
-let cityData =cities;
+//create a polyline using the line coordinates and make the line red.
+L.polyline(line,{
+    color: "blue",
+    smoothFactor: 0.5,
+    weight: 4,
+    dashArray: "10",
+    dashOffset:"10",
+    opacity: 0.5
+}).addTo(map);
 
-//loop thru the cities array and create one marker for each city
-cityData.forEach(function(city){
-    console.log(city);
-    L.circleMarker(city.location, {
-        radius: city.population/200000,
-        color: "orange"
-    })
-    .bindPopup("<h2>"+city.city + ", "+city.state+"</h2> <hr> <h3>Population "+city.population.toLocaleString() +"</h3>")
-    .addTo(map);
-})
-
-// L.circleMarker([34.0522, -118.2437],{
-//     radius: 300,
-//     color: "black",
-//     fillColor: "#ffffa1"
-// }).addTo(map);
 
 //create the tile layer that will be the bakcground of our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    opacity: 0.5,
     accessToken: API_KEY
 });
 
