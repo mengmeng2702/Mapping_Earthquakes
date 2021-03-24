c3507c070e26ff31397fa30fe81b4e3d3324101d
Mengdi Liu <liumengdi923@gmail.com>
2021-03-23 21:12:08 -0400

    added layers to the map to toggle between and popups

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,35 +1,90 @@
 //add console.log to check to see if code is working.
 console.log("working");
 
-//create the map object with a  center and zoom level.
-let map = L.map("mapid").setView([36.1733, -96.5400],5);
-
-//coordinates for each point to be used in the line.
-let line =[
-    [33.9416, -118.4085],
-    [30.1975, -97.6664],
-    [43.6777, -79.6248],
-    [40.6413, -73.7781]
-];
-
-//create a polyline using the line coordinates and make the line red.
-L.polyline(line,{
-    color: "blue",
-    smoothFactor: 0.5,
-    weight: 4,
-    dashArray: "10",
-    dashOffset:"10",
-    opacity: 0.5
-}).addTo(map);
-
-
 //create the tile layer that will be the bakcground of our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    opacity: 0.5,
+    opacity: 0.8,
     accessToken: API_KEY
 });
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+//create a base layer that holds both maps
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+
+//create the map object with center, zoom level and default layer.
+let map = L.map("mapid",{
+    center: [30,30],
+    zoom: 2,
+    layers: [streets]
+})
+
+//Pass our map layers into out layers control and add the layers control to the map
+L.control.layers(baseMaps).addTo(map);
+
+// // Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection",
+//         "features":[{
+//             "type":"Feature",
+//             "properties":{
+//                 "id":"3469",
+//                 "name":"San Francisco International Airport",
+//                 "city":"San Francisco",
+//                 "country":"United States",
+//                 "faa":"SFO",
+//                 "icao":"KSFO",
+//                 "alt":"13",
+//                 "tz-offset":"-8",
+//                 "dst":"A",
+//                 "tz":"America/Los_Angeles"},
+//             "geometry":{
+//                 "type":"Point",
+//                 "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+// //grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport,{
+//     //we turn each feature into a marker on the map
+//     pointToLayer: function(feature,latlng){
+//         console.log(feature);
+//         return L.marker(latlng)
+//         .bindPopup("<h2>"+feature.properties.name + "</h2><hr><h3>"+feature.properties.city+", "+ feature.properties.country);
+//     }
+// }).addTo(map);
+
+// //grabbing our GeoJSON data
+// L.geoJSON(sanFranAirport,{
+//     onEachFeature: function(feature,layer){
+//         console.log(layer);
+//         layer.bindPopup();
+//     }
+// }).addTo(map);
+
+
+//accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/mengmeng2702/Mapping_Earthquakes/main/majorAirports.json"
+//grabbing our GeoJSON data
+d3.json(airportData).then(function(data){
+    console.log(data);
+    //creating a GeoJSON layer with the retrieved data
+    L.geoJson(data,{
+        pointToLayer: function(feature, latlng){
+            return L.marker(latlng)
+            .bindPopup("<h2>Airport code: "+feature.properties.faa+"</h2><hr><h3>Airport name: "+feature.properties.name);
+        }
+    }).addTo(map);
+
+})
 
 //then add our graymap tile layer to the map
 streets.addTo(map);
